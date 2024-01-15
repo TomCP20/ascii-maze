1f803a16d0deb70766b6250b255faf409387f392
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-01-15 17:49:54 +0000

    Added Command Line arguments for user input

--- Program.cs
+++ Program.cs
@@ -2,10 +2,24 @@
 {
     internal static class Program
     {
-        static void Main()
+        static int Main(string[] args)
         {
-            Maze m = MazeGenerator.GenerateMaze(100, 100);
+            int height = Convert.ToInt32(args[0]);
+            int width = Convert.ToInt32(args[1]);
+            string path = "";
+            bool saveMaze = false;
+            if (args.Length > 2)
+            {
+                path = args[2];
+                saveMaze = true;
+            }
+            Maze m = MazeGenerator.GenerateMaze(height, width);
             m.displayMaze();
+            if (saveMaze)
+            {
+                File.WriteAllText(path, m.ToString());
+            }
+            return 0;
         }
     }
 }
